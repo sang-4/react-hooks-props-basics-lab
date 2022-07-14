845dacea98140a00e1dc2ff48e9b684e205b4ecf
Allan <kipropsallan@gmail.com>
2022-07-14 19:11:27 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,15 @@ import About from "./About";
 import user from "../data/user";
 console.log(user);
 
+
+
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
+    <NavBar />
+    <Home name = {user.name} city = {user.city} color = {user.color}/>
+    <About bio = {user.bio} links = {user.links}/>
+  </div>
   );
 }
 
--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "Allan",
+  city: "Nairobi",
   bio: "I made this!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://github.com/sang-4",
+    linkedin: "https://www.linkedin.com/in/allan-kiprop-6ab385157/",
   },
 };
 
